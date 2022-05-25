9b2d9dd04fae81311017a6c894492d60cde23f1f
DavidQF555 <davidfeng2003@gmail.com>
2022-05-24 23:27:50 -0700

    Added Basic Materials

---
+++ src/main/java/io/github/davidqf555/minecraft/yggdrasil/common/Yggdrasil.java
@@ -0,0 +1,23 @@
+package io.github.davidqf555.minecraft.yggdrasil.common;
+
+import io.github.davidqf555.minecraft.yggdrasil.common.registration.ItemRegistry;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("yggdrasil")
+public class Yggdrasil {
+
+    public static final String ID = "yggdrasil";
+
+    public Yggdrasil() {
+        addRegistries(FMLJavaModLoadingContext.get().getModEventBus());
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void addRegistries(IEventBus bus) {
+        ItemRegistry.ITEMS.register(bus);
+    }
+
+}
---
+++ src/main/java/io/github/davidqf555/minecraft/yggdrasil/common/registration/ItemRegistry.java
@@ -0,0 +1,24 @@
+package io.github.davidqf555.minecraft.yggdrasil.common.registration;
+
+import io.github.davidqf555.minecraft.yggdrasil.common.Yggdrasil;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.function.Supplier;
+
+public final class ItemRegistry {
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Yggdrasil.ID);
+
+    public static final RegistryObject<Item> NIFLIUM = register("niflium", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> MUSPELLIUM = register("muspellium", () -> new Item(new Item.Properties()));
+
+    private ItemRegistry() {
+    }
+
+    private static <T extends Item> RegistryObject<T> register(String name, Supplier<T> item) {
+        return ITEMS.register(name, item);
+    }
+}
