1f9903323ac7a7d4333f8a3e0325ed9c345a6f3c
DavidQF555 <davidfeng2003@gmail.com>
2022-05-25 18:02:46 -0700

    Changed to Damage Scaling Effects

--- src/main/java/io/github/davidqf555/minecraft/yggdrasil/common/events/ItemEventSubscriber.java
+++ src/main/java/io/github/davidqf555/minecraft/yggdrasil/common/events/ItemEventSubscriber.java
@@ -9,6 +9,7 @@ import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -25,13 +26,18 @@ public final class ItemEventSubscriber {
         Entity entity = source.getEntity();
         if (entity instanceof LivingEntity && !source.isProjectile() && !source.getMsgId().equals("thrown") && !source.isMagic() && !source.isExplosion() && !source.isFire() && entity.equals(source.getDirectEntity())) {
             Item hand = ((LivingEntity) entity).getItemInHand(Hand.MAIN_HAND).getItem();
+            float damage = event.getAmount();
             LivingEntity target = event.getEntityLiving();
             if (TagRegistry.MUSPELLIUM_TOOLS.contains(hand)) {
-                target.setSecondsOnFire(5);
+                target.setSecondsOnFire(MathHelper.ceil(damage));
             }
             if (TagRegistry.NIFLIUM_TOOLS.contains(hand)) {
                 EffectInstance slow = target.getEffect(Effects.MOVEMENT_SLOWDOWN);
-                target.addEffect(new EffectInstance(Effects.MOVEMENT_SLOWDOWN, 100, slow == null ? 0 : slow.getAmplifier() + 1));
+                int duration = MathHelper.ceil(damage * 20);
+                if (slow != null && slow.getDuration() > duration) {
+                    duration = slow.getDuration();
+                }
+                target.addEffect(new EffectInstance(Effects.MOVEMENT_SLOWDOWN, duration, slow == null ? 0 : slow.getAmplifier() + 1));
             }
         }
     }
